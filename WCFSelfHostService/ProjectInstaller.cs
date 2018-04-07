c7cf442068e892b2dc4669e57466a9168039beec
Pascal Trahan <ptrahan@hotmail.com>
2018-04-07 18:51:30 +0200

    Installer

---
+++ WCFSelfHostService/ProjectInstaller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WCFSelfHostService
+{
+    [RunInstallerAttribute(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBeforeInstall(IDictionary savedState)
+        {
+            string parameter = "WCFSelfHostService\" \"WCFSelfHostServiceLog";
+            Context.Parameters["assemblypath"] = "\"" + Context.Parameters["assemblypath"] + "\" \"" + parameter + "\"";
+            base.OnBeforeInstall(savedState);
+        }
+
+        private void ServiceInstaller_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
